e8de97b27f79cd2d620b96a60dc643d8cb5df28c
datalater <the7mincheol@gmail.com>
2021-10-07 08:33:14 +0900

    feat: prevent re-execute script

--- background.js
+++ background.js
@@ -1,13 +1,19 @@
+let isActivated = false;
+
 chrome.action.onClicked.addListener((tab) => {
   console.log('In Service Worker: toolbar clicked');
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ['content-script.js'],
-  });
+  if (!isActivated) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ['content-script.js'],
+    });
 
-  chrome.scripting.insertCSS({
-    target: { tabId: tab.id },
-    files: ['spotlight.css'],
-  });
+    chrome.scripting.insertCSS({
+      target: { tabId: tab.id },
+      files: ['spotlight.css'],
+    });
+
+    isActivated = true;
+  }
 });
