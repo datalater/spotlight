11d9db0c6b3c308bc23f16dd0303b8ec861f71fa
datalater <the7mincheol@gmail.com>
2022-03-16 20:08:41 +0900

    feat: add toggle UI
    Add a field angle zoom feature.
    
    Refactor to use declararitve component pattern.

--- background.js
+++ background.js
@@ -1,25 +1,15 @@
 const { log } = console;
 
-let isActivated = false;
-
 chrome.action.onClicked.addListener((tab) => {
-  log('In Service Worker: toolbar clicked');
+  log("In Service Worker: toolbar clicked");
 
-  if (!isActivated) {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      files: ['content-script.js'],
-    });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ["content-script.js"],
+  });
 
-    chrome.scripting.insertCSS({
-      target: { tabId: tab.id },
-      files: ['spotlight.css'],
-    });
-
-    isActivated = true;
-  } else {
-    isActivated = false;
-  }
-
-  log(`activated: ${isActivated}`);
+  chrome.scripting.insertCSS({
+    target: { tabId: tab.id },
+    files: ["spotlight.css"],
+  });
 });
